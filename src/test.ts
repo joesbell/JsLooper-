7aa7eec17a457f2a38a7cd10009eade86f3fca6c
joesbell <joesbell@163.com>
2022-04-27 15:01:51 +0800

    ???

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 class GetObj<T> {
   constructor(private name: T) {}
   getName(): string {
-    return this.name + 'hellowrold'
+    return this.name + ''
   }
 }
 // const obj = new GetObj('joes')
